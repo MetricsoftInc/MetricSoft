9c8aaa431f1199258ec8be8b44d0c70862d6ff49
Amy Warren <PLATO\Amy>
2015-09-23 21:58:10 +0530

    Automated Page Security test page

---
+++ AAM/Metricsoft/SQMWebsite/Automated/AutomatedTest.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AutomatedTest.aspx.cs" Inherits="SQM.Website.Automated.AutomatedTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Automated Test</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Literal ID="ltrStatus" runat="server"></asp:Literal>
+    </div>
+    </form>
+</body>
+</html>
---
+++ AAM/Metricsoft/SQMWebsite/Automated/AutomatedTest.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Configuration;
+using System.Net;
+
+namespace SQM.Website.Automated
+{
+	public partial class AutomatedTest : System.Web.UI.Page
+	{
+		static PSsqmEntities entities;
+		static StringBuilder output;
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			output = new StringBuilder();
+
+			//string currentIP = GetIP4Address();
+			string currentIP = GetIPAddress();
+			WriteLine("IP: " + currentIP);
+
+			ltrStatus.Text = output.ToString().Replace("\n", "<br/>");
+			WriteLogFile();
+		}
+
+		public string GetIP4Address()
+		{
+			string IP4Address = String.Empty;
+
+			foreach (IPAddress IPA in Dns.GetHostAddresses(Request.ServerVariables["REMOTE_ADDR"].ToString()))
+			{
+				if (IPA.AddressFamily.ToString() == "InterNetwork")
+				{
+					IP4Address = IPA.ToString();
+					break;
+				}
+			}
+
+			if (IP4Address != String.Empty)
+			{
+				return IP4Address;
+			}
+
+			foreach (IPAddress IPA in Dns.GetHostAddresses(Dns.GetHostName()))
+			{
+				try
+				{
+					WriteLine("IP: " + IPA.ToString() + "; Family: " + IPA.AddressFamily.ToString() + "<br>");
+				}
+				catch { }
+				if (IPA.AddressFamily.ToString() == "InterNetwork")
+				{
+					IP4Address = IPA.ToString();
+					//break;
+				}
+			}
+
+			return IP4Address;
+		}
+
+		public string GetIPAddress ()
+		{
+			string hostName = Dns.GetHostName(); // Retrive the Name of HOST
+			// Get the IP
+			string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+			WriteLine("IP: " + myIP.ToString() + "; Host: " + hostName.ToString());
+			return myIP;
+		}
+		static void WriteLogFile()
+		{
+			try
+			{
+				string logPath = HttpContext.Current.Server.MapPath("~") + "\\log\\";
+				if (!Directory.Exists(logPath))
+					Directory.CreateDirectory(logPath);
+
+				// Write log file
+				string fullPath = logPath + string.Format("{0:yyyy-MM-dd-HHmmssfff}.txt", DateTime.Now);
+				File.WriteAllText(fullPath, output.ToString());
+
+				// Keep only last 100 log files
+				int maxFiles = 100;
+				var info = new DirectoryInfo(logPath);
+				FileInfo[] files = info.GetFiles("*.txt").OrderBy(f => f.CreationTime).ToArray();
+				if (files.Count() > maxFiles)
+					for (int i = 0; i < files.Count() - maxFiles; i++)
+						File.Delete(logPath + files[i].Name);
+			}
+			catch (Exception ex)
+			{
+				WriteLine("WriteLogFile Error: " + ex.ToString());
+				WriteLine("WriteLogFile Detailed Error: " + ex.InnerException.ToString());
+			}
+		}
+
+		static void WriteLine(string text)
+		{
+			output.AppendLine(text);
+		}
+
+	}
+}
